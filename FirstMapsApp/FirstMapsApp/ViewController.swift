88effa6364feb49d632471c7c999a6ff1f431ba6
uturuncuayaku <57707223+uturuncuayaku@users.noreply.github.com>
2021-04-08 10:52:56 -0500

    Add files via upload

---
+++ FirstMapsApp/FirstMapsApp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FirstMapsApp
+//
+//  Created by Andres on 3/28/21.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let austin = MKPointAnnotation()
+        austin.coordinate = CLLocationCoordinate2DMake(30.25, -97.75)
+        austin.title = "Austin"
+        mapView.addAnnotation(austin)
+        
+        let tulsa = MKPointAnnotation()
+        tulsa.coordinate = CLLocationCoordinate2DMake(36.1540, -95.9928)
+        tulsa.title = "Tulsa, Oklahoma"
+        mapView.addAnnotation(tulsa)
+        
+        let nyc = MKPointAnnotation()
+        nyc.coordinate = CLLocationCoordinate2DMake(40.7128, -74.0060)
+        nyc.title = "New York City"
+        mapView.addAnnotation(nyc)
+        
+
+    }
+
+}
+
